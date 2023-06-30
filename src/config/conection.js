45220ad13520bd1761dfe6a9071c4a54d888d54d
deepsarvadhi <135838087+deepsarvadhi@users.noreply.github.com>
2023-06-30 09:34:15 +0530

    some update

--- src/config/conection.js
+++ src/config/conection.js
@@ -9,9 +9,7 @@ const pool = new Pool({
 });
 
 pool.connect((err) => {
-    if (err) {
-        console.log(err);
-    }
+    if (err) throw err;
 })
 
 var table = 'create table if not exists state(ID SERIAL PRIMARY KEY,s_name varchar(20) unique )';
